da28986181dfe75edc398deb34b1cef6cb2acbda
2dayistheday <kmk3075@gmail.com>
2015-06-03 05:28:29 +0900

    step 추가

--- CookCook/app/src/main/java/com/example/s/cookcook/WriteActivity.java
+++ CookCook/app/src/main/java/com/example/s/cookcook/WriteActivity.java
@@ -1,7 +1,11 @@
 package com.example.s.cookcook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by moon3075 on 2015-06-02.
@@ -10,5 +14,22 @@ public class WriteActivity extends Activity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        Button btn_start=(Button)findViewById(R.id.btn_start);
+        final int REQUEST_CODE_NEXT=2000;
+
+        View.OnClickListener listener= new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_start:
+                        Intent intent = new Intent(getBaseContext(), StepActivity.class);
+                        startActivityForResult(intent, REQUEST_CODE_NEXT);
+                        break;
+                }
+            }
+        };
+        btn_start.setOnClickListener(listener);
+
     }
 }
