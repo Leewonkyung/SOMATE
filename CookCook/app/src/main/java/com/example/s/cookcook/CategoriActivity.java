0b9ea991d7bed028a59f9d91c5fd57571097c974
Leewonkyung <wonk0312@naver.com>
2015-06-17 14:06:04 +0900

    카테고리 Activity 전환
    카테고리별 Activity 추가하려고 만듬

--- CookCook/app/src/main/java/com/example/s/cookcook/CategoriActivity.java
+++ CookCook/app/src/main/java/com/example/s/cookcook/CategoriActivity.java
@@ -1,14 +1,30 @@
 package com.example.s.cookcook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
- * Created by Wonkyung on 2015-06-02.
+ * Created by Wonkyung on 2015-06-01.
  */
 public class CategoriActivity extends Activity{
+    ImageButton imageButton1;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categori);
+
+        imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CategoriActivity.this, RiceActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
