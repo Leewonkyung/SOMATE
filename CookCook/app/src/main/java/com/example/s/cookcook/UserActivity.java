af639b3b109529d4922bb0e0700dff6e93f2830f
DahyeK <dahyek24@gmail.com>
2015-06-18 20:38:39 +0900

    갤러리이미지불러오기, 폴더추가버튼
    성공쓰

--- CookCook/app/src/main/java/com/example/s/cookcook/UserActivity.java
+++ CookCook/app/src/main/java/com/example/s/cookcook/UserActivity.java
@@ -1,15 +1,71 @@
 package com.example.s.cookcook;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 /**
  * Created by Wonkyung on 2015-06-01.
  */
 public class UserActivity extends Activity{
+
+    //private static final int PICK_FROM_CAMERA = 1;
+    private static final int PICK_FROM_GALLERY = 2;
+    private ImageView imgview;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
+
+        Button folder_add_button=(Button)findViewById(R.id.folder_add_button);
+        Button user_setting=(Button)findViewById(R.id.user_setting);
+        imgview = (ImageView) findViewById(R.id.user_image);
+
+        folder_add_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LinearLayout addlayout = (LinearLayout)findViewById(R.id.addlayout1);
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.activity_addfolder,addlayout,true);
+            }
+        });
+
+        //setting click -> image select
+        user_setting.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.putExtra("crop", "true");
+                intent.putExtra("aspectX", 0);
+                intent.putExtra("aspectY", 0);
+                intent.putExtra("outputX", 200);
+                intent.putExtra("outputY", 150);
+                try {
+                    intent.putExtra("return-data", true);
+                    startActivityForResult(Intent.createChooser(intent,
+                            "Complete action using"), PICK_FROM_GALLERY);
+                } catch (ActivityNotFoundException e) {
+                    // Do nothing for now
+                }
+            }
+        });
     }
 
+    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
+        if (requestCode == PICK_FROM_GALLERY) {
+            Bundle extras2 = data.getExtras();
+            if (extras2 != null) {
+                Bitmap photo = extras2.getParcelable("data");
+                imgview.setImageBitmap(photo);
+            }
+        }
+    }
 }
